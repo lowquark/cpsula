3816629ccdff091742a79532c90ad7d8ca25ff73
David Petrizze <dpetrizze@gmail.com>
2021-05-05 08:14:40 -0700

    Add return value type warning, minor cleanup

--- src/script.c
+++ src/script.c
@@ -55,20 +55,20 @@ scr_env * scr_env_new() {
   luaL_openlibs(env->L);
 
   if(luaL_loadfile(env->L, main_file) == LUA_OK) {
-    // Argument 1: thread id (zero for now until multithreaded)
-    lua_pushnumber(env->L, 0);
-
-    if(pcall_debug_traceback(env->L, 1, 1) == LUA_OK) {
+    if(pcall_debug_traceback(env->L, 0, 1) == LUA_OK) {
+      // Set handler to return value
       env->handler_fn_ref = luaL_ref(env->L, LUA_REGISTRYINDEX);
     } else {
-      log_error("Lua error while executing '%s'", main_file);
+      // Notify error
+      log_error("Error in %s", main_file);
       log_info("%s", lua_tostring(env->L, -1));
-      lua_pop(env->L, 1); // error object
+      lua_pop(env->L, 1);
     }
   } else {
-    log_error("Failed to load '%s'", main_file);
+    // Notify error
+    log_error("Failed to load %s", main_file);
     log_info("%s", lua_tostring(env->L, -1));
-    lua_pop(env->L, 1); // error message
+    lua_pop(env->L, 1);
   }
 
   assert(lua_gettop(env->L) == 0);
@@ -171,6 +171,8 @@ void scr_reqhandler_execute(scr_reqhandler * handler,
   int return_type;
 
   assert(handler);
+  assert(handler->result_ref == LUA_REFNIL);
+  assert(handler->chunkcb_ref == LUA_REFNIL);
   assert(authority);
   assert(resource);
 
@@ -192,20 +194,27 @@ void scr_reqhandler_execute(scr_reqhandler * handler,
     }
 
     if(pcall_debug_traceback(env->L, 4, 1) == LUA_OK) {
+      // Switch on return value type
       return_type = lua_type(env->L, -1);
-
       if(return_type == LUA_TSTRING || return_type == LUA_TNUMBER) {
+        // Set result to return value
         handler->result_ref = luaL_ref(env->L, LUA_REGISTRYINDEX);
       } else if(return_type == LUA_TFUNCTION) {
+        // Set chunk callback to return value
         handler->chunkcb_ref = luaL_ref(env->L, LUA_REGISTRYINDEX);
-
+        // Call the chunk callback to produce first value
         scr_reqhandler_continue(handler);
       } else {
-        handler->result_ref = LUA_REFNIL;
-        lua_pop(env->L, 1); // return value
+        if(return_type != LUA_TNIL) {
+          log_warning(
+              "Request handler returned value of type %s, expected string, function, or nil",
+              lua_typename(env->L, return_type));
+        }
+        // Ignore return value
+        lua_pop(env->L, 1);
       }
     } else {
-      log_error("Lua error while executing request handler");
+      log_error("Error during request handler");
       log_info("%s", lua_tostring(env->L, -1));
       lua_pop(env->L, 1); // error object
     }
@@ -225,33 +234,40 @@ void scr_reqhandler_continue(scr_reqhandler * handler) {
   assert(env);
 
   if(handler->chunkcb_ref != LUA_REFNIL) {
-    // Set last result to nil
-    luaL_unref(handler->env->L, LUA_REGISTRYINDEX, handler->result_ref);
-    handler->result_ref = LUA_REFNIL;
-
     // Push/call chunk callback
     lua_rawgeti(env->L, LUA_REGISTRYINDEX, handler->chunkcb_ref);
     if(pcall_debug_traceback(env->L, 0, 1) == LUA_OK) {
+      // Switch on return value type
       return_type = lua_type(env->L, -1);
-
       if(return_type == LUA_TSTRING || return_type == LUA_TNUMBER) {
         // Set result to return value
         handler->result_ref = luaL_ref(env->L, LUA_REGISTRYINDEX);
       } else {
+        if(return_type != LUA_TNIL) {
+          log_warning(
+              "Chunk callback returned value of type %s, expected string, function, or nil",
+              lua_typename(env->L, return_type));
+        }
         // Ignore return value
         lua_pop(env->L, 1);
         // Unset chunk callback
         luaL_unref(handler->env->L, LUA_REGISTRYINDEX, handler->chunkcb_ref);
         handler->chunkcb_ref = LUA_REFNIL;
+        // Unset result
+        luaL_unref(handler->env->L, LUA_REGISTRYINDEX, handler->result_ref);
+        handler->result_ref = LUA_REFNIL;
       }
     } else {
       // Notify error
-      log_error("Lua error while executing chunk callback");
+      log_error("Error during chunk callback");
       log_info("%s", lua_tostring(env->L, -1));
       lua_pop(env->L, 1);
       // Unset chunk callback
       luaL_unref(handler->env->L, LUA_REGISTRYINDEX, handler->chunkcb_ref);
       handler->chunkcb_ref = LUA_REFNIL;
+      // Unset result
+      luaL_unref(handler->env->L, LUA_REGISTRYINDEX, handler->result_ref);
+      handler->result_ref = LUA_REFNIL;
     }
   }
 
